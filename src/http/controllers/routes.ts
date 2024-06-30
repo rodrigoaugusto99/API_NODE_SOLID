bed0e2dc8ef254942023ba2ae5fd164a6d237b01
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-06-30 16:26:41 -0300

    authenticate: testes e controller

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './register';
+import { authenticate } from './authenticate';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
+
+  app.post('/sessions', authenticate);
 }

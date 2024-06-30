365ef96ba4e4fd36e8ffc8d8dc671e3946159d7e
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-06-29 22:33:03 -0300

    repository pattern

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,26 +1,31 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma';
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
+import { hash } from 'bcryptjs';
 
 interface RegisterUseCaseRequest {
-    name: string,
-    email: string,
-    password: string,
+  name: string;
+  email: string;
+  password: string;
 }
 
-export async function registerUseCase({name, email, password} : RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6);
 
-  const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+  const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
 
   if (userWithSameEmail) {
-    throw new Error('User with same email already exists.')
+    throw new Error('User with same email already exists.');
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  });
 }

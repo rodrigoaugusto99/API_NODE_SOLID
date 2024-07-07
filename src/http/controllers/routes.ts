1af3b2e31f42a6373ea2259bdd52a9401bc802f6
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-07-07 18:05:18 -0300

    add: middleware

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -1,9 +1,13 @@
-import { FastifyInstance } from 'fastify';
-import { register } from './register';
-import { authenticate } from './authenticate';
+import { FastifyInstance } from "fastify";
+import { register } from "./register";
+import { authenticate } from "./authenticate";
+import { verifyJwt } from "./middlewares/verify-jwt";
+import { profile } from "./profile";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', register);
+  app.post("/users", register);
 
-  app.post('/sessions', authenticate);
+  app.post("/sessions", authenticate);
+
+  app.get("/me", { onRequest: [verifyJwt] }, profile);
 }

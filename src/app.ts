6970d2aad9dd6a9a3dd0cbca3c83dc5263f648d7
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-07-07 17:03:04 -0300

    authentication things

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import fastify from 'fastify';
-import { appRoutes } from './http/controllers/routes';
-import { ZodError } from 'zod';
-import { env } from './env';
+import fastify from "fastify";
+import { appRoutes } from "./http/controllers/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes);
 
@@ -11,14 +16,14 @@ app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: 'Validation error.', issues: error.format() });
+      .send({ message: "Validation error.", issues: error.format() });
   }
 
-  if (env.NODE_ENV !== 'production') {
+  if (env.NODE_ENV !== "production") {
     console.error(error);
   } else {
     // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
 
-  return reply.status(500).send({ message: 'Internal server error.' });
+  return reply.status(500).send({ message: "Internal server error." });
 });
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,7 +1,7 @@
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
-import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case';
-import { FastifyReply, FastifyRequest } from 'fastify';
-import { z } from 'zod';
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 
 export async function authenticate(
   request: FastifyRequest,
@@ -17,9 +17,22 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
     });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
@@ -28,6 +41,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }

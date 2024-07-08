201a9b29812bda65910a889557375c3a7251a204
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-07-08 00:14:20 -0300

    organizando scripts e configurando ambiente para testes e2e

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { app } from './app';
+import { app } from "./app";
 
 app
   .listen({
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP Server Running!');
+    console.log("HTTP Server Running!");
   });

ad28e144fbdf069f10d42303d6b908b0d2f2e580
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-07-08 23:46:23 -0300

    organizando rotas + gyms e checkins controllers

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,12 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
-import { verifyJwt } from "../middlewares/verify-jwt";
 import { profile } from "./profile";
+import { verifyJwt } from "@/http/middlewares/verify-jwt";
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
-
   app.post("/sessions", authenticate);
 
   app.get("/me", { onRequest: [verifyJwt] }, profile);
